907f6efe96433b115085f50560b73a198fe2ed7f
prateeksonii <prateek_soni@outlook.com>
2021-11-07 16:39:07 +0530

    add auth routes

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,16 +1,17 @@
 const Joi = require('joi');
 const argon2 = require('argon2');
+const jwt = require('jsonwebtoken');
 const User = require('../models/User');
-
-const userSchema = Joi.object({
-  firstName: Joi.string().required().trim().normalize(),
-  lastName: Joi.string().trim().normalize(),
-  email: Joi.string().email().required().trim(),
-  password: Joi.string().min(4),
-});
 
 exports.createUser = async (req, res, next) => {
   const { firstName, lastName, email, password } = req.body;
+
+  const userSchema = Joi.object({
+    firstName: Joi.string().required().trim().normalize(),
+    lastName: Joi.string().trim().normalize(),
+    email: Joi.string().email().required().trim(),
+    password: Joi.string().required().min(4),
+  });
 
   try {
     const { value, error } = userSchema.validate({
@@ -43,6 +44,123 @@ exports.createUser = async (req, res, next) => {
       result: {
         user,
       },
+    });
+  } catch (err) {
+    return next(err);
+  }
+};
+
+exports.signIn = async (req, res, next) => {
+  const { email, password } = req.body;
+  const signInSchema = Joi.object({
+    email: Joi.string().email().required().trim(),
+    password: Joi.string().required(),
+  });
+
+  try {
+    const { value, error } = signInSchema.validate({
+      email,
+      password,
+    });
+
+    if (error) {
+      res.status(403);
+      throw new Error(error.details.map((detail) => detail.message).join(','));
+    }
+
+    const [user] = await User.query().select('*').where('email', value.email);
+
+    if (!user) {
+      res.status(403);
+      throw new Error('No user found with this email address');
+    }
+
+    const isValidPassword = await argon2.verify(user.password, value.password);
+
+    if (!isValidPassword) {
+      res.status(403);
+      throw new Error('Invalid Email or Password');
+    }
+
+    delete user.password;
+
+    const token = jwt.sign({ user }, process.env.JWT_SECRET, {
+      expiresIn: '10y',
+    });
+
+    return res.status(200).json({
+      ok: true,
+      result: {
+        token,
+        user,
+      },
+    });
+  } catch (err) {
+    return next(err);
+  }
+};
+
+exports.isAuthenticated = (req, res, next) => {
+  try {
+    const { authorization } = req.headers;
+
+    if (!authorization) {
+      return res.json({
+        ok: false,
+        user: null,
+      });
+    }
+
+    const [, token] = authorization.split(' ');
+
+    if (!token) {
+      res.status(401);
+      throw new Error('Invalid token');
+    }
+
+    const { user } = jwt.verify(token, process.env.JWT_SECRET);
+
+    if (!user) {
+      res.status(401);
+      throw new Error('Invalid token');
+    }
+
+    req.user = user;
+
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+};
+
+exports.getSignedInUser = (req, res, next) => {
+  try {
+    const { authorization } = req.headers;
+
+    if (!authorization) {
+      return res.json({
+        ok: false,
+        user: null,
+      });
+    }
+
+    const [, token] = authorization.split(' ');
+
+    if (!token) {
+      res.status(401);
+      throw new Error('Invalid token');
+    }
+
+    const { user } = jwt.verify(token, process.env.JWT_SECRET);
+
+    if (!user) {
+      res.status(401);
+      throw new Error('Invalid token');
+    }
+
+    return res.json({
+      ok: true,
+      user,
     });
   } catch (err) {
     return next(err);
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,6 +1,12 @@
 const router = require('express').Router();
-const { createUser } = require('../controllers/userController');
+const {
+  createUser,
+  signIn,
+  getSignedInUser,
+} = require('../controllers/userController');
 
 router.post('/', createUser);
+router.post('/signin', signIn);
+router.get('/', getSignedInUser);
 
 module.exports = router;
